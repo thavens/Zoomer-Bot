ba93d38046aed3b0d265c48df8fb6e686cb09af2
Thavens <michael.lavery.2017@gmail.com>
2021-04-23 12:56:16 -0700

    remove excess paths

--- config.py
+++ config.py
@@ -1,10 +1,9 @@
 import os
-import secret
-zoom_username = secret.user
-zoom_password = secret.pass
+import secret #remove import when using for yourself
+zoom_username = secret.user #replace with zoom account username
+zoom_password = secret.pass #replace with zoom account password
 thresh = 5 #this is at least how many people have to leave in 5 seconds to trigger logout
-tesseract_path = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
-zoom_path = os.path.expanduser('~') + '/AppData/Roaming/Zoom/bin/Zoom.exe' #path to zoom application
+
 webcam = False
 webcam_downscale_factor = 5 #i.e. enter 2 to display webcam at 1/2 resolution
 video_file = 0 #path + file for virtual webcam recording. enter number to play real webcam. (default = 0)
